b398de1495b768700e3495a80dd5e979ec968df1
Shekhar Roy <43708251+Shekhar-roy-5292@users.noreply.github.com>
2024-08-21 17:33:36 +0530

    Add Album image title section

---
+++ Components/PlayerRepeatToggle.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PlayerRepeatToggle = () => {
+  return (
+    <View>
+      <Text>PlayerRepeatToggle</Text>
+    </View>
+  )
+}
+
+export default PlayerRepeatToggle
+
+const styles = StyleSheet.create({})
--- Scr/Screen/PlayerScreen.js
+++ Scr/Screen/PlayerScreen.js
@@ -1,15 +1,22 @@
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import Feather from 'react-native-vector-icons/Feather';
 import {colors} from '../Constants/colors';
-import {iconSizes, spacing} from '../Constants/dimensions';
+import {fontSize, iconSizes, spacing} from '../Constants/dimensions';
 import {fontFamilies} from '../Constants/fonts';
 import {useNavigation} from '@react-navigation/native';
+import PlayerRepeatToggle from '../../Components/PlayerRepeatToggle';
 const PlayerScreen = () => {
   const navigation = useNavigation();
+  const isLiked = false;
+  const isMute = false;
+  const imageUrl =
+    'https://linkstorage.linkfire.com/medialinks/images/2bd3689a-7679-4e8b-9f5a-b8b1b73f567d/artwork-440x440.jpg';
   return (
     <SafeAreaView style={styles.container}>
+      {/* header */}
       <View style={styles.headerContainer}>
         <TouchableOpacity onPress={() => navigation.goBack()}>
           <AntDesign
@@ -19,6 +26,39 @@ const PlayerScreen = () => {
           />
         </TouchableOpacity>
         <Text style={styles.headerText}>Playing Now</Text>
+      </View>
+      {/* image */}
+      <View style={styles.coverImageContainer}>
+        <Image source={{uri: imageUrl}} style={styles.coverImage} />
+      </View>
+      {/* render the title and artist */}
+      <View style={styles.titleRowHeartContainer}>
+        {/* title row container */}
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>On & On (feat. Daniel Levi)</Text>
+          <Text style={styles.artist}>Alan Walker</Text>
+        </View>
+        {/* icon container */}
+        <TouchableOpacity>
+          <AntDesign
+            name={isLiked ? 'heart' : 'hearto'}
+            color={colors.iconSecondary}
+            size={iconSizes.md}
+          />
+        </TouchableOpacity>
+      </View>
+      {/* controls  */}
+      <View style={styles.playerControlContainer}>
+        <TouchableOpacity>
+          <Feather
+            name={isMute ? 'volume-x' : 'volume-1'}
+            color={colors.iconSecondary}
+            size={iconSizes.lg}
+          />
+        </TouchableOpacity>
+        <View>
+          <PlayerRepeatToggle />
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -45,4 +85,34 @@ const styles = StyleSheet.create({
     fontFamily: fontFamilies.medium,
     fontSize: iconSizes.md,
   },
+  coverImageContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: spacing.xl,
+  },
+  coverImage: {
+    height: 260,
+    width: 260,
+    borderRadius: 10,
+  },
+  titleContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: fontSize.xl,
+    color: colors.textPrimary,
+    fontFamily: fontFamilies.medium,
+  },
+  artist: {
+    fontSize: fontSize.md,
+    color: colors.textSecondary,
+  },
+  titleRowHeartContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  playerControlContainer: {},
 });
