ea9e21100dfab4489221b7a0d8b02fcbfa106545
Shekhar Roy <43708251+Shekhar-roy-5292@users.noreply.github.com>
2024-08-22 15:48:14 +0530

    set drawer navigate

---
+++ AppNavigation/CustomDrawerContent.js
@@ -0,0 +1,144 @@
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react';
+import {
+  DrawerContentScrollView,
+  DrawerItem,
+  DrawerItemList,
+} from '@react-navigation/drawer';
+import {colors} from '../Scr/Constants/colors';
+import {fontSize, iconSizes, spacing} from '../Scr/Constants/dimensions';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Octicons from 'react-native-vector-icons/Octicons';
+import {useNavigation} from '@react-navigation/native';
+import {fontFamilies} from '../Scr/Constants/fonts';
+
+const CustomDrawerContent = props => {
+  const isDarkMode = true;
+  return (
+    <DrawerContentScrollView style={styles.container}>
+      {/* <DrawerItemList {...props} /> */}
+      <View style={styles.headerIconContainer}>
+        <TouchableOpacity onPress={() => props.navigation.closeDrawer()}>
+          <AntDesign
+            name="close"
+            size={iconSizes.lg}
+            color={colors.iconPrimary}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Octicons
+            name={isDarkMode ? 'sun' : 'moon'}
+            size={iconSizes.lg}
+            color={colors.iconPrimary}
+          />
+        </TouchableOpacity>
+      </View>
+      {/* drawer items */}
+      <View style={styles.drawerItemContainer}>
+        <DrawerItem
+          label={'Profile'}
+          icon={() => (
+            <FontAwesome
+              name="user-circle"
+              size={iconSizes.md}
+              color={colors.iconSecondary}
+            />
+          )}
+          labelStyle={styles.labelStyle}
+          style={styles.drawerItem}
+        />
+        <DrawerItem
+          label={'Liked Songs'}
+          icon={() => (
+            <AntDesign
+              name="hearto"
+              size={iconSizes.md}
+              color={colors.iconSecondary}
+            />
+          )}
+          labelStyle={styles.labelStyle}
+          style={styles.drawerItem}
+          onPress={() => props.navigation.navigate('Like')}
+        />
+        <DrawerItem
+          label={'Language'}
+          icon={() => (
+            <FontAwesome
+              name="language"
+              size={iconSizes.md}
+              color={colors.iconSecondary}
+            />
+          )}
+          labelStyle={styles.labelStyle}
+          style={styles.drawerItem}
+          onPress={() => props.navigation.navigate('Like')}
+        />
+        <DrawerItem
+          label={'Contact Us'}
+          icon={() => (
+            <FontAwesome
+              name="envelope-o"
+              size={iconSizes.md}
+              color={colors.iconSecondary}
+            />
+          )}
+          labelStyle={styles.labelStyle}
+          style={styles.drawerItem}
+          onPress={() => props.navigation.navigate('Like')}
+        />
+        <DrawerItem
+          label={'FAQs'}
+          icon={() => (
+            <FontAwesome
+              name="question-circle-o"
+              size={iconSizes.md}
+              color={colors.iconSecondary}
+            />
+          )}
+          labelStyle={styles.labelStyle}
+          style={styles.drawerItem}
+          onPress={() => props.navigation.navigate('Like')}
+        />
+        <DrawerItem
+          label={'Settings'}
+          icon={() => (
+            <FontAwesome
+              name="cog"
+              size={iconSizes.md}
+              color={colors.iconSecondary}
+            />
+          )}
+          labelStyle={styles.labelStyle}
+          style={styles.drawerItem}
+          onPress={() => props.navigation.navigate('Like')}
+        />
+      </View>
+    </DrawerContentScrollView>
+  );
+};
+
+export default CustomDrawerContent;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.background,
+    padding: spacing.lg,
+  },
+  headerIconContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  drawerItemContainer: {
+    marginVertical: spacing.xl,
+  },
+  labelStyle: {
+    fontSize: fontSize.md,
+    color: colors.textPrimary,
+    fontFamily: fontFamilies.medium,
+  },
+  drawerItem: {
+    marginVertical: spacing.sm,
+  },
+});
--- AppNavigation/DrawNavigation.js
+++ AppNavigation/DrawNavigation.js
@@ -4,12 +4,15 @@ import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import StackNavigation from './StackNavigation';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import CustomDrawerContent from './CustomDrawerContent';
 export default function DrawNavigation() {
   const Drawer = createDrawerNavigator();
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <NavigationContainer>
-        <Drawer.Navigator screenOptions={{headerShown: false}}>
+        <Drawer.Navigator
+          screenOptions={{headerShown: false}}
+          drawerContent={props => <CustomDrawerContent {...props} />}>
           <Drawer.Screen name="Stack" component={StackNavigation} />
         </Drawer.Navigator>
       </NavigationContainer>
